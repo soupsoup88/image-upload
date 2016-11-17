ab90372b27d8222de9e02422ff361f495c17cf79
Nikhil Tiware <NikhilTi@housing.ufl.edu>
2016-11-17 09:32:22 -0500

    first commit

---
+++ app/js/config.js
@@ -0,0 +1,5 @@
+photoAlbumApp.config(['cloudinaryProvider', function (cloudinaryProvider) {
+  cloudinaryProvider
+      .set("cloud_name", "dhfbyx8mm")
+      .set("upload_preset", "imyicsee");
+}]);
